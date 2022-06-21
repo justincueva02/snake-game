5fde1d3b4c20acf9c673217542c236b7509fdc28
Justin Cueva <justincueva@Justins-MacBook-Pro.local>
2022-06-21 12:04:39 -0700

    displaying snake that is 3 blocks in length

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('this is the snake game')
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+
+tim = Turtle()
+
+
+def init_turtle(arg):
+    new_turtle = Turtle()
+    new_turtle.color('#fff')
+    new_turtle.shape('square')
+    new_turtle.setpos(arg * 20, 0)
+
+
+number_of_turtles = 3
+for x in range(number_of_turtles):
+    init_turtle(x)
+
+
+screen.exitonclick()
