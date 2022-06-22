5e859b81e7670a187fdefaca3d6185a9dc2a1554
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-21 17:39:15 -0700

    moving the snake

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('My Snake Game')
+screen.tracer(0)
 
-tim = Turtle()
+starting_positions = [(0, 0), (-20, 0), (-40, 0), (-60, 0), (-80, 0)]
+segments = []
 
 
-def init_turtle(arg):
+def init_turtle(coordinates):
     new_turtle = Turtle()
-    new_turtle.color('#fff')
+    new_turtle.penup()
     new_turtle.shape('square')
-    new_turtle.setpos(arg * 20, 0)
+    new_turtle.color('#fff')
+    new_turtle.goto(coordinates)
+    segments.append(new_turtle)
 
 
-number_of_turtles = 3
-for x in range(number_of_turtles):
-    init_turtle(x)
+for position in starting_positions:
+    init_turtle(position)
 
-
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+    # for x in range(6):
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
 screen.exitonclick()
