2ae3c94259cfb654498241ddb4f47f7481e50352
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-21 18:22:31 -0700

    moving the snake with the arrrows

--- snake.py
+++ snake.py
@@ -1,15 +1,20 @@
 from turtle import Turtle
 
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
 
 class Snake:
     def __init__(self):
-        self.starting_positions = [(0, 0), (-20, 0), (-40, 0), (-60, 0), (-80, 0)]
+        self.starting_positions = STARTING_POSITIONS
         self.segments = []
+        self.create_snake()
 
+    def create_snake(self):
         for position in self.starting_positions:
-            self.init_turtle(position)
+            self.create_segment(position)
 
-    def init_turtle(self, coordinates):
+    def create_segment(self, coordinates):
         new_turtle = Turtle()
         new_turtle.penup()
         new_turtle.shape('square')
@@ -22,4 +27,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.segments[0].setheading(90)
+        print('up')
+
+    def down(self):
+        self.segments[0].setheading(270)
+        print('down')
+
+    def left(self):
+        self.segments[0].setheading(180)
+        print('left')
+
+    def right(self):
+        self.segments[0].setheading(0)
+        print('right')
